e6ab02740cf2210081f3b9380bea6cf78003239a
MrDave 1502 <djbuhagiar9@gmail.com>
2023-11-02 20:32:00 +0100

    Initial commit, part of feature 1

--- src/main/java/com/weatherwear/WeatherWearApp.java
+++ src/main/java/com/weatherwear/WeatherWearApp.java
@@ -5,7 +5,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import org.json.
 public class WeatherWearApp {
 
     public static void main (String args[]) throws IOException, InterruptedException {
