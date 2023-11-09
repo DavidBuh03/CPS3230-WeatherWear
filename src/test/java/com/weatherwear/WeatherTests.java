b44c46e46f60d5094202d68ac1a94fe12d30a5a9
MrDave 1502 <djbuhagiar9@gmail.com>
2023-11-09 22:46:05 +0100

    finished all unit tests

--- src/test/java/com/weatherwear/WeatherTests.java
+++ src/test/java/com/weatherwear/WeatherTests.java
@@ -2,22 +2,156 @@ package com.weatherwear;
 
 import com.weatherwear.stubs.IpServiceStubs;
 import com.weatherwear.stubs.WeatherServiceStubs;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 
 public class WeatherTests {
     Weather weather;
+    IpServiceStubs ipService;
+    WeatherServiceStubs weatherService;
 
-    //final int TEMPERATURE_LESS_THAN_FIFTEEN_DEGREES = 4;
-    //final int TEMPERATURE_FIFTEEN_DEGREES = 15;
-    //final int TEMPERATURE_HIGHER_THAN_FIFTEEN_DEGREES = 16;
+    final float TEMPERATURE_LESS_THAN_FIFTEEN_DEGREES = 4;
+    final float TEMPERATURE_FIFTEEN_DEGREES = 15;
+    final float TEMPERATURE_HIGHER_THAN_FIFTEEN_DEGREES = 15.1F;
+    final float PRECIPITATION_ZERO = 0;
+    final float PRECIPITATION_NONZERO = 0.1F;
 
     @BeforeEach
     public void setup() {
         weather = new Weather();
-        weather.setIpService(new IpServiceStubs());
-        weather.setWeatherService(new WeatherServiceStubs());
+        ipService = new IpServiceStubs();
+        weatherService = new WeatherServiceStubs();
+        weather.setIpService(ipService);
+        weather.setWeatherService(weatherService);
     }
+
+    @Test
+    public void testConvertingWeatherNowJsonToWeatherResultObject() {
+        //Setup
+        WeatherResult testResult;
+        //Exercise
+        testResult = weather.convertWeatherNowToWeatherResult(weather.weatherService.requestWeatherNow(1, "test"));
+        //Verify
+        Assertions.assertNotNull(testResult);
+        //Teardown
+    }
+    @Test
+    public void testConvertingWeatherForecastJsonToWeatherResultObject() {
+        //Setup
+        WeatherResult testResult;
+        //Exercise
+        testResult = weather.convertWeatherForecastToWeatherResult(weather.weatherService.requestWeatherForecast("test", "test"));
+        //Verify
+        Assertions.assertNotNull(testResult);
+        //Teardown
+    }
+
+    @Test
+    public void testGetWeatherNow() {
+        //Setup
+        WeatherResult testResult;
+        //Exercise
+        testResult = weather.getWeatherNow();
+        //Verify
+        Assertions.assertNotNull(testResult);
+
+        Assertions.assertEquals(1, ipService.getIpAddressCallCount);
+        Assertions.assertEquals(1, weatherService.requestWeatherNowCallCount);
+        //Teardown
+    }
+    @Test
+    public void testGetWeatherForecast() {
+        //Setup
+        WeatherResult testResult;
+        //Exercise
+        testResult = weather.getWeatherForecast("test", "test");
+        //Verify
+        Assertions.assertNotNull(testResult);
+        Assertions.assertEquals(1, weatherService.requestWeatherForecastCallCount);
+        //Teardown
+    }
+
+    @Test
+    public void testRecommendationColdWeatherNoRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_LESS_THAN_FIFTEEN_DEGREES, PRECIPITATION_ZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertTrue(recommendation.clothingCold);
+        Assertions.assertFalse(recommendation.umbrella);
+        //Teardown
+
+    }
+    @Test
+    public void testRecommendationColdWeatherFifteenDegreesNoRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_FIFTEEN_DEGREES, PRECIPITATION_ZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertTrue(recommendation.clothingCold);
+        Assertions.assertFalse(recommendation.umbrella);
+        //Teardown
+
+    }
+    @Test
+    public void testRecommendationWarmWeatherNoRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_HIGHER_THAN_FIFTEEN_DEGREES, PRECIPITATION_ZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertFalse(recommendation.clothingCold);
+        Assertions.assertFalse(recommendation.umbrella);
+        //Teardown
+
+    }
+    @Test
+    public void testRecommendationColdWeatherWithRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_LESS_THAN_FIFTEEN_DEGREES, PRECIPITATION_NONZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertTrue(recommendation.clothingCold);
+        Assertions.assertTrue(recommendation.umbrella);
+        //Teardown
+
+    }
+    @Test
+    public void testRecommendationColdWeatherFifteenDegreesWithRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_FIFTEEN_DEGREES, PRECIPITATION_NONZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertTrue(recommendation.clothingCold);
+        Assertions.assertTrue(recommendation.umbrella);
+        //Teardown
+
+    }
+    @Test
+    public void testRecommendationWarmWeatherWithRain() {
+        //Setup
+        WeatherResult testResult = new WeatherResult(TEMPERATURE_HIGHER_THAN_FIFTEEN_DEGREES, PRECIPITATION_NONZERO);
+        Recommendation recommendation;
+        //Exercise
+        recommendation = weather.recommend(testResult);
+        //Verify
+        Assertions.assertFalse(recommendation.clothingCold);
+        Assertions.assertTrue(recommendation.umbrella);
+        //Teardown
+
+    }
+
 
 
 
--- src/test/java/com/weatherwear/stubs/WeatherServiceStubs.java
+++ src/test/java/com/weatherwear/stubs/WeatherServiceStubs.java
@@ -5,11 +5,11 @@ import com.google.gson.JsonObject;
 import com.weatherwear.services.WeatherService;
 
 public class WeatherServiceStubs implements WeatherService {
-
+    //these stubs also work as spies because i can only inject one dependency per interface
     public int requestWeatherNowCallCount = 0;
     public int requestWeatherForecastCallCount = 0;
     String weatherNowSampleOutput = "{\"location\":{\"name\":\"Tarxien\",\"region\":\"\",\"country\":\"Malta\"},\"current\":{\"temp_c\":21.0,\"precip_mm\":0.05}}";
-    String weatherForecastSampleOutput = "{\"location\":{\"name\":\"Luqa\",\"region\":\"\",\"country\":\"Malta\"},\"forecast\":{\"forecastday\":[{\"day\":{\"avgtemp_c\":20.8,\"totalprecip_mm\":2.62}]}}";
+    String weatherForecastSampleOutput = "{\"location\":{\"name\":\"Luqa\",\"region\":\"\",\"country\":\"Malta\"},\"forecast\":{\"forecastday\":[{\"day\":{\"avgtemp_c\":20.8,\"totalprecip_mm\":2.62}}]}}";
     Gson gson = new Gson();
     @Override
     public JsonObject requestWeatherNow(int service, String ip) {
